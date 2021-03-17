2dbcb8e130c4d93ff320ad383e0ed4fde3af60f4
Flavio Castelli <fcastelli@suse.com>
2021-03-17 12:39:15 +0100

    Add settings validation capabilities

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+use anyhow::anyhow;
+
 pub mod request;
 pub mod response;
+pub mod settings;
 pub mod test;
 
 use crate::response::*;
@@ -23,4 +26,37 @@ pub fn reject_request(message: Option<String>, code: Option<u16>) -> wapc_guest:
         message,
         code,
     })?)
+}
+
+/// waPC guest function to register under the name `validate_settings`
+/// # Example
+//
+/// ```
+/// // Settings is a user defined Setting struct
+/// register_function("validate_settings", validate_settings::<Settings>);
+/// ```
+pub fn validate_settings<T>(payload: &[u8]) -> wapc_guest::CallResult
+where
+    T: serde::de::DeserializeOwned + settings::Validatable,
+{
+    let settings: T = serde_json::from_slice::<T>(payload).map_err(|e| {
+        anyhow!(
+            "Error decoding validation payload {}: {:?}",
+            String::from_utf8_lossy(payload),
+            e
+        )
+    })?;
+
+    let res = match settings.validate() {
+        Ok(_) => settings::SettingsValidationResponse {
+            valid: true,
+            message: None,
+        },
+        Err(e) => settings::SettingsValidationResponse {
+            valid: false,
+            message: Some(e.to_string()),
+        },
+    };
+
+    Ok(serde_json::to_vec(&res)?)
 }
---
+++ src/settings.rs
@@ -0,0 +1,16 @@
+/// Trait that must be implemented by setting
+/// object
+pub trait Validatable {
+    /// Ensures the values given by the user are valid
+    fn validate(&self) -> Result<(), String>;
+}
+
+/// A SettingsValidationResponse object holds the outcome of settings
+/// validation.
+#[derive(serde::Serialize, Debug)]
+pub struct SettingsValidationResponse {
+    /// True if the settings are valid
+    pub valid: bool,
+    /// Message shown to the user when the settings are not valid
+    pub message: Option<String>,
+}
