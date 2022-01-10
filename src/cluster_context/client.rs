60adcff47a92c47514956747fbf7ec6283edcc94
Flavio Castelli <fcastelli@suse.com>
2022-01-10 18:28:58 +0100

    Fix tests execution for context aware policies
    The context aware code must be updated to have a specific non-wasm
    implementation. Otherwise the link step will fail because of unresolved
    symbols required by wapc-guest.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

---
+++ src/cluster_context/client.rs
@@ -0,0 +1,80 @@
+use anyhow::{anyhow, Result};
+use k8s_openapi::api::core::v1::{Namespace, Service};
+use k8s_openapi::api::networking::v1::Ingress;
+use wapc_guest as guest;
+
+pub trait Client {
+    /// Get list of namespaces
+    fn namespaces(&self) -> Result<Vec<u8>>;
+
+    /// Get list of ingresses
+    fn ingresses(&self) -> Result<Vec<u8>>;
+
+    /// Get list of services
+    fn services(&self) -> Result<Vec<u8>>;
+}
+
+pub struct WapcClient {}
+
+impl Client for WapcClient {
+    fn namespaces(&self) -> Result<Vec<u8>> {
+        guest::host_call("kubernetes", "namespaces", "list", &Vec::new())
+            .map_err(|e| anyhow!("{}", e))
+    }
+
+    fn ingresses(&self) -> Result<Vec<u8>> {
+        guest::host_call("kubernetes", "ingresses", "list", &Vec::new())
+            .map_err(|e| anyhow!("{}", e))
+    }
+
+    fn services(&self) -> Result<Vec<u8>> {
+        guest::host_call("kubernetes", "services", "list", &Vec::new())
+            .map_err(|e| anyhow!("{}", e))
+    }
+}
+
+/// Fake client used when running unit tests. This should be used when writing
+/// code that doesn't target wasm32
+pub struct TestClient {
+    /// Mock list of ingresses that the waPC fake host will return.
+    pub mock_ingresses: Result<Vec<Ingress>>,
+
+    /// Mock list of namespaces that the waPC fake host will return.
+    pub mock_namespaces: Result<Vec<Namespace>>,
+
+    /// Mock list of services that the waPC fake host will return.
+    pub mock_services: Result<Vec<Service>>,
+}
+
+impl Default for TestClient {
+    fn default() -> Self {
+        TestClient {
+            mock_ingresses: Ok(vec![]),
+            mock_namespaces: Ok(vec![]),
+            mock_services: Ok(vec![]),
+        }
+    }
+}
+
+impl Client for TestClient {
+    fn namespaces(&self) -> Result<Vec<u8>> {
+        match &self.mock_namespaces {
+            Ok(v) => Ok(serde_json::to_vec(&v).unwrap()),
+            Err(e) => Err(anyhow!("{}", e)),
+        }
+    }
+
+    fn ingresses(&self) -> Result<Vec<u8>> {
+        match &self.mock_ingresses {
+            Ok(v) => Ok(serde_json::to_vec(&v).unwrap()),
+            Err(e) => Err(anyhow!("{}", e)),
+        }
+    }
+
+    fn services(&self) -> Result<Vec<u8>> {
+        match &self.mock_services {
+            Ok(v) => Ok(serde_json::to_vec(&v).unwrap()),
+            Err(e) => Err(anyhow!("{}", e)),
+        }
+    }
+}
---
+++ src/cluster_context/mod.rs
@@ -0,0 +1,174 @@
+extern crate wapc_guest as guest;
+
+use anyhow::{anyhow, Result};
+
+use k8s_openapi::api::core::v1::{Namespace, Service};
+use k8s_openapi::api::networking::v1::Ingress;
+use k8s_openapi::List;
+
+mod client;
+
+/// A `ClusterContext` allows a waPC guest policy to retrieve cluster
+/// contextual information from a Kubernetes cluster.
+///
+/// Right now a set of well known resources is hardcoded, but the idea
+/// is to generalize this so the SDK can support any kind of
+/// Kubernetes resource and custom resource definition.
+pub struct ClusterContext {
+    client: Box<dyn client::Client>,
+}
+
+impl Default for ClusterContext {
+    #[cfg(target_arch = "wasm32")]
+    fn default() -> Self {
+        use self::client::WapcClient;
+
+        ClusterContext {
+            client: Box::new(WapcClient {}),
+        }
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn default() -> Self {
+        use self::client::TestClient;
+
+        ClusterContext {
+            client: Box::new(TestClient::default()),
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum NamespaceFilter {
+    AllNamespaces,
+    Namespace(String),
+}
+
+impl ClusterContext {
+    pub fn new_with_client(client: Box<dyn client::Client>) -> Self {
+        ClusterContext { client }
+    }
+
+    /// Return the list of `Ingress` resources that exist in the
+    /// cluster.
+    pub fn ingresses(&self, namespace: NamespaceFilter) -> Result<Vec<Ingress>> {
+        // TODO (ereslibre): use macros to remove duplication and then
+        // generalize
+        Ok(self
+            .client
+            .ingresses()
+            .map_err(|err| anyhow!("failed to call ingresses binding: {}", err))
+            .and_then(|ingresses| {
+                Ok(
+                    serde_json::from_str::<List<Ingress>>(std::str::from_utf8(&ingresses)?)
+                        .map_err(|err| anyhow!("failed to unmarshal ingress list: {}", err))?
+                        .items,
+                )
+            })?
+            .iter()
+            .filter_map(|ingress| match &namespace {
+                NamespaceFilter::AllNamespaces => Some(ingress.clone()),
+                NamespaceFilter::Namespace(namespace_filter) => {
+                    if let Some(ingress_namespace) = &ingress.metadata.namespace {
+                        if namespace_filter == ingress_namespace {
+                            Some(ingress.clone())
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                }
+            })
+            .collect())
+    }
+
+    /// Return the list of `Namespace` resources that exist in the
+    /// cluster.
+    pub fn namespaces(&self) -> Result<Vec<Namespace>> {
+        // TODO (ereslibre): use macros to remove duplication and then
+        // generalize
+        self.client
+            .namespaces()
+            .map_err(|err| anyhow!("failed to call namespaces binding: {}", err))
+            .and_then(|namespaces| {
+                Ok(
+                    serde_json::from_str::<List<Namespace>>(std::str::from_utf8(&namespaces)?)
+                        .map_err(|err| anyhow!("failed to unmarshal namespace list: {}", err))?
+                        .items,
+                )
+            })
+    }
+
+    /// Return the list of `Service` resources that exist in the
+    /// cluster.
+    pub fn services(&self, namespace: NamespaceFilter) -> Result<Vec<Service>> {
+        // TODO (ereslibre): use macros to remove duplication and then
+        // generalize
+        Ok(self
+            .client
+            .services()
+            .map_err(|err| anyhow!("failed to call services binding: {}", err))
+            .and_then(|services| {
+                Ok(
+                    serde_json::from_str::<List<Service>>(std::str::from_utf8(&services)?)
+                        .map_err(|err| anyhow!("failed to unmarshal service list: {}", err))?
+                        .items,
+                )
+            })?
+            .iter()
+            .filter_map(|service| match &namespace {
+                NamespaceFilter::AllNamespaces => Some(service.clone()),
+                NamespaceFilter::Namespace(namespace_filter) => {
+                    if let Some(service_namespace) = &service.metadata.namespace {
+                        if namespace_filter == service_namespace {
+                            Some(service.clone())
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                }
+            })
+            .collect())
+    }
+}
+
+impl ClusterContext {
+    /// Return a specific ingress object with a given name and a
+    /// namespace filter. If the namespace filter is broad, more than
+    /// one resource might be returned.
+    pub fn ingress(&self, namespace: NamespaceFilter, name: &str) -> Result<Vec<Ingress>> {
+        // TODO (ereslibre): use macros to remove duplication and then
+        // generalize
+        Ok(self
+            .ingresses(namespace)?
+            .into_iter()
+            .filter(|ingress| ingress.metadata.name == Some(name.to_string()))
+            .collect())
+    }
+
+    // Return a specific namespace with a given name.
+    pub fn namespace(&self, name: &str) -> Result<Option<Namespace>> {
+        // TODO (ereslibre): use macros to remove duplication and then
+        // generalize
+        Ok(self
+            .namespaces()?
+            .into_iter()
+            .find(|namespace| namespace.metadata.name == Some(name.to_string())))
+    }
+
+    /// Return a specific service object with a given name and a
+    /// namespace filter. If the namespace filter is broad, more than
+    /// one resource might be returned.
+    pub fn service(&self, namespace: NamespaceFilter, name: &str) -> Result<Vec<Service>> {
+        // TODO (ereslibre): use macros to remove duplication and then
+        // generalize
+        Ok(self
+            .services(namespace)?
+            .into_iter()
+            .filter(|service| service.metadata.name == Some(name.to_string()))
+            .collect())
+    }
+}
