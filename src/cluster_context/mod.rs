aecbccd0a12641b7090daa1493c48e13bcef459f
Flavio Castelli <fcastelli@suse.com>
2022-01-11 10:16:12 +0100

    Improve docs for context aware policies
    Provide more details about how to use the SDK, including testing
    instructions.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- src/cluster_context/mod.rs
+++ src/cluster_context/mod.rs
@@ -1,3 +1,13 @@
+//! This module provides a way for policies to gather information from a
+//! Kubernetes cluster. The policies making use of this feature are called
+//! "context-aware" policies.
+//!
+//! > **Warning:** currently (Jan 2022), this feature is still considered alpha.
+//! > Because of that the API is subject to changes.
+//!
+//! Details about Kubernetes resources already existing inside of the cluster can
+//! be obtained via the [`ClusterContext`] object.
+
 extern crate wapc_guest as guest;
 
 use anyhow::{anyhow, Result};
@@ -6,7 +16,7 @@ use k8s_openapi::api::core::v1::{Namespace, Service};
 use k8s_openapi::api::networking::v1::Ingress;
 use k8s_openapi::List;
 
-mod client;
+pub mod client;
 
 /// A `ClusterContext` allows a waPC guest policy to retrieve cluster
 /// contextual information from a Kubernetes cluster.
@@ -14,6 +24,37 @@ mod client;
 /// Right now a set of well known resources is hardcoded, but the idea
 /// is to generalize this so the SDK can support any kind of
 /// Kubernetes resource and custom resource definition.
+///
+/// ## Usage inside of policies
+///
+/// When using `ClusterContext` inside of a policy, there's no need to
+/// cache its instance via something like [`lazy_static`](https://crates.io/crates/lazy_static).
+/// Allocating the `ClusterContext` is cheap, both in terms of cpu cycles and
+/// memory.
+///
+/// An instance of `ClusterContext` can be created in this way:
+///
+/// ```
+/// use kubewarden_policy_sdk::cluster_context::ClusterContext;
+///
+/// let cluster_cts = ClusterContext::default();
+/// ```
+///
+/// ## Usage inside of unit tests
+///
+/// `ClusterContext` cannot behave the same way when its used inside of Wasm code
+/// and native code (like when running policy tests via `cargo test`).
+///
+/// When running inside of unit tests, there's no communication with a real Kubernetes
+/// cluster. The Kubernetes resources returned by the `ClusterContext` instance
+/// are mocks defined inside of a [`client::TestClient`] object.
+///
+/// The implementation of `ClusterContext::default()` automatically allocates
+/// a default [`client::TestClient`] object that returns successful empty responses.
+///
+/// If you want to provide ad-hoc responses (including communication errors), please
+/// build the `ClusterContext` using the [`ClusterContext::new_with_client`] method.
+///
 pub struct ClusterContext {
     client: Box<dyn client::Client>,
 }
@@ -45,6 +86,12 @@ pub enum NamespaceFilter {
 }
 
 impl ClusterContext {
+    /// This method is available only when the code is **not** build for the Wasm
+    /// target.
+    ///
+    /// This method is meant to be used only when writing unit tests, so that
+    /// an instance of [`client::TestClient`] can be used to provide mock results.
+    #[cfg(not(target_arch = "wasm32"))]
     pub fn new_with_client(client: Box<dyn client::Client>) -> Self {
         ClusterContext { client }
     }
