397ed3cfdb76ca41278d787c452596ac1b197c13
Raul Cabello Martin <raulcabm@gmail.com>
2022-04-21 16:08:40 +0200

    Add comments
    Co-authored-by: Flavio Castelli <flavio@castelli.me>

--- src/host_capabilities/mod.rs
+++ src/host_capabilities/mod.rs
@@ -20,15 +20,17 @@ pub enum CallbackRequestType {
     SigstorePubKeyVerify {
         /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
         image: String,
-        /// The configuration to use at verification time
+        /// List of PEM encoded keys that must have been used to sign the OCI object
         pub_keys: Vec<String>,
+        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
         annotations: Option<HashMap<String, String>>,
     },
     SigstoreKeylessVerify {
         /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
         image: String,
-        /// The configuration to use at verification time
+        /// List of keyless signatures that must be found
         keyless: Vec<KeylessInfo>,
+        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
         annotations: Option<HashMap<String, String>>,
     },
 }
