d6a4461c72d238550128333620a34d22c745fe72
raulcabello <raul.cabello@suse.com>
2022-07-11 11:35:16 +0200

    feat: Add SigstoreVerificationInputV1{}
    Co-authored-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>
    Co-authored-by: raulcabello <raul.cabello@suse.com>

--- src/host_capabilities/mod.rs
+++ src/host_capabilities/mod.rs
@@ -6,6 +6,51 @@ pub mod net;
 pub mod oci;
 pub mod verification;
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum SigstoreVerificationInputV1 {
+    /// Require the verification of the manifest digest of an OCI object (be
+    /// it an image or anything else that can be stored into an OCI registry)
+    /// to be signed by Sigstore, using public keys mode
+    SigstorePubKeyVerify {
+        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
+        image: String,
+        /// List of PEM encoded keys that must have been used to sign the OCI object
+        pub_keys: Vec<String>,
+        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
+        annotations: Option<HashMap<String, String>>,
+    },
+
+    // Require the verification of the manifest digest of an OCI object to be
+    // signed by Sigstore, using keyless mode
+    SigstoreKeylessVerify {
+        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
+        image: String,
+        /// List of keyless signatures that must be found
+        keyless: Vec<KeylessInfo>,
+        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
+        annotations: Option<HashMap<String, String>>,
+    },
+}
+
+impl Into<CallbackRequestType> for SigstoreVerificationInputV1{
+    fn into(self) -> CallbackRequestType {
+        match self {
+            SigstoreVerificationInputV1::SigstorePubKeyVerify { image, pub_keys, annotations} => {CallbackRequestType::SigstorePubKeyVerify {
+                image,
+                pub_keys,
+                annotations
+            }},
+            SigstoreVerificationInputV1::SigstoreKeylessVerify { image, keyless, annotations } => {
+                CallbackRequestType::SigstoreKeylessVerify {image, keyless, annotations}
+            }
+        }
+    }
+}
+// internal tag
+pub enum SigstoreVerificationInputV2 {
+
+}
+
 /// Describes the different kinds of request a waPC guest can make to
 /// our host.
 #[derive(Serialize, Deserialize, Debug)]
