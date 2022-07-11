d25e8da0dec265822bfe4175376d0f63731bb84d
raulcabello <raul.cabello@suse.com>
2022-07-11 16:15:21 +0200

    Move CallbackRequestType{} from sdk to policy-evaluator
    Use SigstoreVerificationInputV2{} instead of CallbackRequestType{} for
    validation calls.
    
    Co-authored-by: raulcabello <raul.cabello@suse.com>
    Co-authored-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- src/host_capabilities/mod.rs
+++ src/host_capabilities/mod.rs
@@ -32,21 +32,6 @@ pub enum SigstoreVerificationInputV1 {
     },
 }
 
-impl Into<CallbackRequestType> for SigstoreVerificationInputV1{
-    fn into(self) -> CallbackRequestType {
-        match self {
-            SigstoreVerificationInputV1::SigstorePubKeyVerify { image, pub_keys, annotations} => {CallbackRequestType::SigstorePubKeyVerify {
-                image,
-                pub_keys,
-                annotations
-            }},
-            SigstoreVerificationInputV1::SigstoreKeylessVerify { image, keyless, annotations } => {
-                CallbackRequestType::SigstoreKeylessVerify {image, keyless, annotations}
-            }
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum SigstoreVerificationInputV2 {
@@ -96,83 +81,5 @@ pub enum SigstoreVerificationInputV2 {
         repo: Option<String>,
         /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
         annotations: Option<HashMap<String, String>>,
-    }
-}
-
-impl From<SigstoreVerificationInputV2> for CallbackRequestType{
-    fn from(val: SigstoreVerificationInputV2) -> Self {
-        match val {
-            SigstoreVerificationInputV2::SigstorePubKeyVerify { image, pub_keys, annotations } =>
-                CallbackRequestType::SigstorePubKeyVerify {image, pub_keys, annotations},
-            SigstoreVerificationInputV2::SigstoreKeylessVerify { image, keyless, annotations } =>
-                CallbackRequestType::SigstoreKeylessVerify {image, keyless, annotations},
-            SigstoreVerificationInputV2::SigstoreKeylessPrefixVerify { image, keyless_prefix, annotations } =>
-                CallbackRequestType::SigstoreKeylessPrefixVerify {image, keyless_prefix, annotations},
-            SigstoreVerificationInputV2::SigstoreGithubActionsVerify { image, owner, repo, annotations } =>
-                CallbackRequestType::SigstoreGithubActionsVerify {image, owner, repo, annotations},
-        }
-    }
-}
-
-/// Describes the different kinds of request a waPC guest can make to
-/// our host.
-#[derive(Serialize, Deserialize, Debug)]
-pub enum CallbackRequestType {
-    /// Require the computation of the manifest digest of an OCI object (be
-    /// it an image or anything else that can be stored into an OCI registry)
-    OciManifestDigest {
-        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
-        image: String,
     },
-
-    /// Require the verification of the manifest digest of an OCI object (be
-    /// it an image or anything else that can be stored into an OCI registry)
-    /// to be signed by Sigstore, using public keys mode
-    SigstorePubKeyVerify {
-        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
-        image: String,
-        /// List of PEM encoded keys that must have been used to sign the OCI object
-        pub_keys: Vec<String>,
-        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
-        annotations: Option<HashMap<String, String>>,
-    },
-
-    // Require the verification of the manifest digest of an OCI object to be
-    // signed by Sigstore, using keyless mode
-    SigstoreKeylessVerify {
-        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
-        image: String,
-        /// List of keyless signatures that must be found
-        keyless: Vec<KeylessInfo>,
-        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
-        annotations: Option<HashMap<String, String>>,
-    },
-
-    // Require the verification of the manifest digest of an OCI object to be
-    // signed by Sigstore using keyless mode, where the passed subject is a URL
-    // prefix of the subject to match
-    SigstoreKeylessPrefixVerify {
-        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
-        image: String,
-        /// List of keyless signatures that must be found
-        keyless_prefix: Vec<KeylessPrefixInfo>,
-        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
-        annotations: Option<HashMap<String, String>>,
-    },
-
-    // Require the verification of the manifest digest of an OCI object to be
-    // signed by Sigstore using keyless mode and performed in GitHub Actions
-    SigstoreGithubActionsVerify {
-        /// String pointing to the object (e.g.: `registry.testing.lan/busybox:1.0.0`)
-        image: String,
-        /// owner of the repository. E.g: octocat
-        owner: String,
-        /// Optional - Repo of the GH Action workflow that signed the artifact. E.g: example-repo
-        repo: Option<String>,
-        /// Optional - Annotations that must have been provided by all signers when they signed the OCI artifact
-        annotations: Option<HashMap<String, String>>,
-    },
-
-    /// Lookup the addresses for a given hostname via DNS
-    DNSLookupHost { host: String },
 }
