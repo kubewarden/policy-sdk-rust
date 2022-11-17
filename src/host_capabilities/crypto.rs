4857dcf17763f4cf15ff2c13845cced1238ef7ce
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2022-11-17 12:42:25 +0100

    feat: Add host_capabilities::crypto::verify_cert()
    This new sdk function verifies a cert against a passed cert_chain.
    
    It uses the new host callback
    `kubewarden/crypto/v1/is_certificate_trusted`.
    
    The new `crypto` callback json request and response are specified in the
    new `crypto_v1` module.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

---
+++ src/host_capabilities/crypto.rs
@@ -0,0 +1,41 @@
+use crate::host_capabilities::crypto_v1::{
+    CertificateVerificationRequest, CertificateVerificationResponse,
+};
+use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
+
+/// A x509 certificate
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct Certificate {
+    /// Which encoding is used by the certificate
+    pub encoding: CertificateEncoding,
+    /// Actual certificate
+    pub data: Vec<u8>,
+}
+
+/// The encoding of the certificate
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub enum CertificateEncoding {
+    #[allow(missing_docs)]
+    Der,
+    #[allow(missing_docs)]
+    Pem,
+}
+
+/// Verify_cert verifies cert against the passed cert_chain
+pub fn verify_cert(cert: Certificate, cert_chain: Option<Vec<Certificate>>) -> Result<bool> {
+    let req = CertificateVerificationRequest { cert, cert_chain };
+    let msg = serde_json::to_vec(&req).map_err(|e| {
+        anyhow!(
+            "error serializing the certificate verification request: {}",
+            e
+        )
+    })?;
+    let response_raw =
+        wapc_guest::host_call("kubewarden", "crypto", "v1/is_certificate_trusted", &msg)
+            .map_err(|e| anyhow!("{}", e))?;
+
+    let response: CertificateVerificationResponse = serde_json::from_slice(&response_raw)?;
+
+    Ok(response.trusted)
+}
