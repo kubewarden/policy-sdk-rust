kubewarden/policy-sdk-rust

commit 3dbead4485f0618129835dbdc2d58fe231ee83f2

fix: make certificate type case-insensetive
Signed-off-by: Khaled Emara <mail@KhaledEmara.dev>

src/host_capabilities/crypto.rs
@@ -15,6 +15,7 @@ pub struct Certificate {
 
 /// The encoding of the certificate
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub enum CertificateEncoding {
     #[allow(missing_docs)]
     Der,

commit 22a2d8284f82d72f0adecb1a61c4268668591b56

bug: fix breaking change done by 0.9.5 release
The 0.9.5 release of this crate broke the `v1/is_certificate_trusted`
host capability.

The break was introduced by commit
3dbead4485f0618129835dbdc2d58fe231ee83f2
which has been reverted by this commit.

Fixes #105

Signed-off-by: Flavio Castelli <fcastelli@suse.com>

src/host_capabilities/crypto.rs
@@ -15,11 +15,17 @@ pub struct Certificate {
 
 /// The encoding of the certificate
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
-#[serde(rename_all = "lowercase")]
 pub enum CertificateEncoding {
     #[allow(missing_docs)]
+    // Be explicit about how the name should be handled
+    // see https://github.com/kubewarden/policy-sdk-rust/issues/105
+    #[serde(rename = "Der")]
     Der,
+
     #[allow(missing_docs)]
+    // Be explicit about how the name should be handled
+    // see https://github.com/kubewarden/policy-sdk-rust/issues/105
+    #[serde(rename = "Pem")]
     Pem,
 }
 